3a409494567ed6f31ce161f9b8b75e7cc957e319
SUBERNER <80803238+SUBERNER@users.noreply.github.com>
2025-09-29 15:02:45 -0500

    Foundation completed
    The basics of the language is done as it is not able to do the following
    correctly use static types and make sure the types are correct type
    separated float, int, and strings.
    ability to declare and assign values.
    for now, this will be all before the
    better error handling.
    This will be all before this weeks milestone

--- pet.py
+++ pet.py
@@ -9,8 +9,8 @@ import random
 class PetStatus:
     # stores the data of each need and how they work
     class Need():
-        def __init__(self, alive: bool, current: float = 1, action: str = '', minmax: tuple[float,float] = tuple[0,2], drain: float = 0.1, gain: float = 0.1, delay: float = 1):
-            self._alive = alive # a reference to the alive value in pet
+        def __init__(self, alive: bool, current: float = 1, action: str = '', minmax: tuple[float, float] = tuple[0, 2], drain: float = 0.1, gain: float = 0.1, delay: float = 1):
+            self._alive = alive  # a reference to the alive value in pet
             self._current = current  # how much of the need is fulfilled
             self._action = action  # the string that notifies the user what need is being worked with
             self._minmax = minmax  # the minimum amount of the need before the pet dies and the maximum amount a need can be satisfied
@@ -32,7 +32,7 @@ class PetStatus:
                     death() # causes pet to die and stopping the program
             # checks if needs are above the maximum. if below the maximum, then cap
             elif self._current > self._minmax[1]:
-                self._current = self._minmax[1] # sets current back within the maximum limits
+                self._current = self._minmax[1]  # sets current back within the maximum limits
 
         def current_calculation(self, value: float = 0, severity: float = 1, offset: tuple[float, float] = tuple[0, 0]):
             # calculations and alterations that can be done to give more power to how current is altered
@@ -74,20 +74,27 @@ class PetStatus:
 #####################################
 
 class PetLexer(Lexer):
-    tokens = {NAME, NUMBER, STRING, TYPE}  # categorizes of each token that will be used
+    tokens = {NAME, INT, FLOAT, STRING, TYPE}  # categorizes of each token that will be used
     ignore = '\t '  # tokens that are ignored by the program
-    literals = {'=', '+', '-', '/', '%',
-                '*', '(', ')', ',', ';'}  # simple tokens that will be required constantly
+    literals = {'=', '+', '-', '*', '/', '%', '^',
+                '(', ')', ',', ';'}  # simple tokens that will be required constantly
 
     # the format of NAME and STRING
     # first [] stores start with characters and second [] stores followed by characters
-    TYPE = r'int|string'
+    TYPE = r'int|float|string'
     NAME = r'[a-zA-Z][a-zA-Z0-9_]*'
     STRING = r'\".*?\"'  # strings must be double quote with anything in it
 
+    # tokens for float numbers
+    @_(r'\d+\.\d+')  # any digits
+    def FLOAT(self, token):
+        # makes value into python format
+        token.value = float(token.value)
+        return token
+
     # tokens for integer numbers
     @_(r'\d+')  # any digits
-    def NUMBER(self, token):
+    def INT(self, token):
         # makes value into python format
         token.value = int(token.value)
         return token
@@ -109,7 +116,7 @@ class PetParser(Parser):
     precedence = (
         ('left', '+', '-'),
         ('left', '*', '/', '%'),
-        ('right', '^'), # exponents
+        ('right', '^'),  # exponents
         ('right', 'UMINUS')
     )
 
@@ -127,9 +134,9 @@ class PetParser(Parser):
         return parse.var_declare
 
     # assigned an expression or operation to the variable with the type if the var is new
-    @_('TYPE NAME "=" EXPR')
+    @_('TYPE NAME "=" expr')
     def var_declare(self, parse):
-        return 'var_declare', parse.TYPE, parse.NAME, parse.EXPR
+        return 'var_declare', parse.TYPE, parse.NAME, parse.expr
 
     # used for variables and assigning variables
     # variables with something assigned to it
@@ -138,64 +145,69 @@ class PetParser(Parser):
         return parse.var_assign
 
     # assigned an expression or operation to the variable
-    @_('NAME "=" EXPR')
+    @_('NAME "=" expr')
     def var_assign(self, parse):
-        return 'var_assign', parse.NAME, parse.EXPR
+        return 'var_assign', parse.NAME, parse.expr
 
     # simply an expression or operation
-    @_('EXPR')
+    @_('expr')
     def statement(self, parse):
-        return parse.EXPR
+        return parse.expr
 
     # expressions and operations when dealing with math-based precedence
     # addition
-    @_('EXPR "+" EXPR')
-    def EXPR(self, parse):
-        return 'add', parse.EXPR0, parse.EXPR1
+    @_('expr "+" expr')
+    def expr(self, parse):
+        return 'add', parse.expr0, parse.expr1
 
     # subtraction
-    @_('EXPR "-" EXPR')
-    def EXPR(self, parse):
-        return 'sub', parse.EXPR0, parse.EXPR1
+    @_('expr "-" expr')
+    def expr(self, parse):
+        return 'sub', parse.expr0, parse.expr1
 
     # multiplication
-    @_('EXPR "*" EXPR')
-    def EXPR(self, parse):
-        return 'mul', parse.EXPR0, parse.EXPR1
+    @_('expr "*" expr')
+    def expr(self, parse):
+        return 'mul', parse.expr0, parse.expr1
 
     # division
-    @_('EXPR "/" EXPR')
-    def EXPR(self, parse):
-        return 'div', parse.EXPR0, parse.EXPR1
+    @_('expr "/" expr')
+    def expr(self, parse):
+        return 'div', parse.expr0, parse.expr1
 
     # remainder
-    @_('EXPR "%" EXPR')
-    def EXPR(self, parse):
-        return 'rem', parse.EXPR0, parse.EXPR1
+    @_('expr "%" expr')
+    def expr(self, parse):
+        return 'rem', parse.expr0, parse.expr1
 
     # exponents
-    @_('EXPR "^" EXPR')
-    def EXPR(self, parse):
-        return 'pow', parse.EXPR0, parse.EXPR1
+    @_('expr "^" expr')
+    def expr(self, parse):
+        return 'pow', parse.expr0, parse.expr1
 
     # negatives
-    @_('"-" EXPR %prec UMINUS')
-    def EXPR(self, parse):
-        return parse.EXPR
+    @_('"-" expr %prec UMINUS')
+    def expr(self, parse):
+        return parse.expr
 
     # simply a name
     @_('NAME')
-    def EXPR(self, parse):
+    def expr(self, parse):
         return 'var', parse.NAME
 
-    # simply a number
-    @_('NUMBER')
-    def EXPR(self, parse):
-        return 'num', parse.NUMBER
+    # simply a float number
+    @_('FLOAT')
+    def expr(self, parse):
+        return 'num', parse.FLOAT
+
+    # simply an int number
+    @_('INT')
+    def expr(self, parse):
+        return 'num', parse.INT
 
     # simply a string
     @_('STRING')
-    def EXPR(self, parse):
+    def expr(self, parse):
         return 'str', parse.STRING
 
 ###########################################
@@ -207,15 +219,15 @@ class PetExecute:
         self.environment = environment  # stores the variables
         result = self.walk(tree)  # returns the full abstract syntax tree holding the split statements form the parser
         # prints results after the tree has been walked and the results of the statements have been made
-        if result is not None and isinstance(result, int):
+        if result is not None and (isinstance(result, int) or isinstance(result, float)):
             print(result)
         if isinstance(result, str) and result[0] == '"':  # test if a result is a string
             print(result)
 
-
     def walk(self, node):
+        print(node)  # DEBUGGING DEBUGGING DEBUGGING DEBUGGING DEBUGGING DEBUGGING
         # returns if node is already a Python-based value
-        if isinstance(node, int) or isinstance(node, str):
+        if isinstance(node, int) or isinstance(node, float) or isinstance(node, str):
             return node
 
         # returns if no nodes where found
@@ -228,28 +240,74 @@ class PetExecute:
 
         # returns the nodes value it after doing simple math
         if node[0] == 'add':
-            return self.walk(node[1]) + self.walk(node[2])
+            node0 = self.walk(node[1])
+            node1 = self.walk(node[2])
+            if type(node0) == type(node1):  # ints, floats, and strings
+                return self.walk(node[1]) + self.walk(node[2])
+            else:  # if there was a type mismatch
+                print(f"\033[31m< '{node[1][1]}' & '{node[2][1]}' Uncombatable >\033[0m")  # displays if variables are uncombatable
         elif node[0] == 'sub':
-            return self.walk(node[1]) - self.walk(node[2])
+            node0 = self.walk(node[1])
+            node1 = self.walk(node[2])
+            if type(node0) == type(node1):  # ints, floats, and strings
+                return self.walk(node[1]) - self.walk(node[2])
+            else:  # if there was a type mismatch
+                print(f"\033[31m< '{node[1][1]}' & '{node[2][1]}' Uncombatable >\033[0m")  # displays if variables are uncombatable
         elif node[0] == 'mul':
-            return self.walk(node[1]) * self.walk(node[2])
+            node0 = self.walk(node[1])
+            node1 = self.walk(node[2])
+            if (isinstance(node0, (int, float)) and type(node0) == type(node1)) or ((isinstance(node0, int) and isinstance(node1, str)) or (isinstance(node0, str) and isinstance(node1, int))):  # ints and floats, or one string and one int
+                return self.walk(node[1]) * self.walk(node[2])
+            else:  # if there was a type mismatch
+                print(f"\033[31m< '{node[1][1]}' & '{node[2][1]}' Uncombatable >\033[0m")  # displays if variables are uncombatable
         elif node[0] == 'div':
-            return self.walk(node[1]) / self.walk(node[2])
+            node0 = self.walk(node[1])
+            node1 = self.walk(node[2])
+            if isinstance(node0, (int, float)) and type(node0) == type(node1):  # ints and floats
+                return self.walk(node[1]) / self.walk(node[2])
+            else:  # if there was a type mismatch
+                print(f"\033[31m< '{node[1][1]}' & '{node[2][1]}' Uncombatable >\033[0m")  # displays if variables are uncombatable
         elif node[0] == 'rem':
-            return self.walk(node[1]) % self.walk(node[2])
+            node0 = self.walk(node[1])
+            node1 = self.walk(node[2])
+            if isinstance(node0, (int, float)) and type(node0) == type(node1):  # ints and floats
+                return self.walk(node[1]) % self.walk(node[2])
+            else:  # if there was a type mismatch
+                print(f"\033[31m< '{node[1][1]}' & '{node[2][1]}' Uncombatable >\033[0m")  # displays if variables are uncombatable
         elif node[0] == 'pow':
-            return self.walk(node[1]) ** self.walk(node[2])
+            node0 = self.walk(node[1])
+            node1 = self.walk(node[2])
+            if isinstance(node0, (int, float)) and type(node0) == type(node1):  # ints and floats
+                return self.walk(node[1]) ** self.walk(node[2])
+            else:  # if there was a type mismatch
+                print(f"\033[31m< '{node[1][1]}' & '{node[2][1]}' Uncombatable >\033[0m")  # displays if variables are uncombatable
 
         # stores data inside variables inside the environment when declared
         if node[0] == 'var_declare':
-            environmental_variable = {'type': node[1], 'value': self.walk(node[3])}
+            # type casting
+            node_variable = self.walk(node[3])  # gets the value for testing and storing in environment
+            if node[1] == 'float' and isinstance(node_variable, int):
+                node_variable = float(node_variable)  # changes int to float
+            elif node[1] == 'int' and isinstance(node_variable, float):
+                print(f"\033[33m< 'Converted '{node_variable}' To '{int(node_variable)}' >\033[0m")  # notifies that float was changed to int
+                node_variable = int(node_variable)  # changes float to int
+            # stores value
+            environmental_variable = {'type': node[1], 'value': node_variable}
             self.environment[node[2]] = environmental_variable
-            return node[1]
+            return node[2]
 
         # returns and stores data inside variables that are stored in the environment
         # only works for variables that already exist
         if node[0] == 'var_assign':
-            self.environment[node[1]]['value'] = self.walk(node[2])
+            # type casting
+            node_variable = self.walk(node[2])  # gets the value for testing and storing in environment
+            if self.environment[node[1]]['type'] == 'float' and isinstance(node_variable, int):  # searches for the variable's type in the environment
+                node_variable = float(node_variable)  # changes int to float
+            elif self.environment[node[1]]['type'] == 'int' and isinstance(node_variable, float):  # searches for the variable's type in the environment
+                print(f"\033[33m< 'Converted '{node_variable}' To '{int(node_variable)}' >\033[0m")  # notifies that float was changed to int
+                node_variable = int(node_variable)  # changes float to int
+            # stores value
+            self.environment[node[1]]['value'] = node_variable
             return node[1]
 
         # returns the value of the variable asked for
@@ -257,8 +315,9 @@ class PetExecute:
             try:
                 return self.environment[node[1]]['value']  # tries to find variable and its data
             except LookupError:  # if no variable or data was found
-                print(f"< '{node[1]}' Undefined >")  # only displays name of variable
+                print(f"\033[31m< '{node[1]}' Undefined >\033[0m")  # only displays name of variable
                 return 0
+
 
 
 ######################
@@ -271,7 +330,7 @@ if __name__ == '__main__':
     parser = PetParser()
     status = PetStatus()
     print(f"Take Care Of Your New Pet")
-    print(f"{status.name} Is Your Pet's Name")
+    print(f"'{status.name}' Is Your Pet's Name")
     environment = {}  # all variables that are kept between each command a user makes
     # environment stores static variables as a dictionary, so it will store the variable type and the value inside the variable
 
@@ -289,12 +348,12 @@ if __name__ == '__main__':
 
         # if commands form user was received
         if command:
-            print(list(lexer.tokenize(command)))  # DEBUGGING ONLY
+            print(list(lexer.tokenize(command)))  # DEBUGGING DEBUGGING DEBUGGING DEBUGGING DEBUGGING DEBUGGING
             tree = parser.parse(lexer.tokenize(command))  # splits command between spaces
             PetExecute(tree, environment)  # runs the commands
 
     # makes user press something ENTER before the program fully closes
-    input(f'< Press ENTER To Exit >')
+    input(f'\033[31< Press ENTER To Exit >\033[0m')
 
 
 
